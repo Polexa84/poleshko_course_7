82cbb929359286cc6a54e125f70e9d6651b5f428
polexa84 <polexa84@gmail.com>
2025-06-22 00:54:18 +0300

    обновлено отбражение и ссылки

--- habits/urls.py
+++ habits/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter
 from .views import HabitViewSet
 
-router = routers.DefaultRouter()
-router.register(r'habits', HabitViewSet)
+router = DefaultRouter()
+router.register(r'habits', HabitViewSet, basename='habit')
 
 urlpatterns = [
     path('', include(router.urls)),
