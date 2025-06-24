bc5c3503eadc4cda944efaee69c64839da6c1f17
polexa84 <polexa84@gmail.com>
2025-06-24 23:30:30 +0300

    Исправлены замечания ❌ Эндпоинт регистрации не возвращает HTTP статус 201, возникает ошибка:
    AttributeError: type object 'Token' has no attribute 'objects'

--- users/views.py
+++ users/views.py
@@ -2,22 +2,19 @@ from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import RegistrationSerializer, LoginSerializer
-from rest_framework.authtoken.models import Token  # Для создания токенов
 
 class RegistrationView(generics.CreateAPIView):
     serializer_class = RegistrationSerializer
     permission_classes = (permissions.AllowAny,)  # Разрешаем всем регистрироваться
 
-    def post(self, request, *args, **kwargs):
+    def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        token, created = Token.objects.get_or_create(user=user)  # Создаем или получаем токен
         return Response({
-            "token": token.key,
             "user_id": user.pk,
             "email": user.email
-        }, status=status.HTTP_201_CREATED)
+        }, status=status.HTTP_201_CREATED)  # Возвращаем 201 Created
 
 
 class LoginView(generics.CreateAPIView):
@@ -28,9 +25,7 @@ class LoginView(generics.CreateAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']  # Получаем пользователя из сериализатора
-        token, created = Token.objects.get_or_create(user=user)  # Создаем или получаем токен
         return Response({
-            "token": token.key,
             "user_id": user.pk,
             "email": user.email
         }, status=status.HTTP_200_OK)
